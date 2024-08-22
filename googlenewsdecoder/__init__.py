f4b110704a12db7c4d2caed701870bdab7d4f9cf
Felipe Hertzer <felipeaugustohertzer@gmail.com>
2024-08-22 16:53:34 +1000

    Added Support to Batch Requests v4

--- googlenewsdecoder/__init__.py
+++ googlenewsdecoder/__init__.py
@@ -1,3 +1,4 @@
 from .decoderv1 import decode_google_news_url as decoderv1
 from .decoderv2 import decode_google_news_url as decoderv2
 from .decoderv3 import decode_google_news_url as decoderv3
+from .decoderv4 import decode_google_news_url as decoderv4
