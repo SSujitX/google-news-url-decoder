d6f3beb984d48bdce2bf128683d5163d9d9a1811
ssujitx <ssujitxx@gmail.com>
2024-08-22 07:04:14 +0600

    Updates

---
+++ googlenewsdecoder/decoderv3.py
@@ -0,0 +1,88 @@
+import requests
+import base64
+
+
+def fetch_decoded_batch_execute(id):
+    try:
+        s = (
+            '[[["Fbv4je","[\\"garturlreq\\",[[\\"en-US\\",\\"US\\",[\\"FINANCE_TOP_INDICES\\",\\"WEB_TEST_1_0_0\\"],'
+            'null,null,1,1,\\"US:en\\",null,180,null,null,null,null,null,0,null,null,[1608992183,723341000]],'
+            '\\"en-US\\",\\"US\\",1,[2,3,4,8],1,0,\\"655000234\\",0,0,null,0],\\"'
+            + id
+            + '\\"]",null,"generic"]]]'
+        )
+
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+            "Referer": "https://news.google.com/",
+        }
+
+        response = requests.post(
+            "https://news.google.com/_/DotsSplashUi/data/batchexecute?rpcids=Fbv4je",
+            headers=headers,
+            data={"f.req": s},
+        )
+
+        if response.status_code != 200:
+            return {"status": False, "error": "Failed to fetch data from Google."}
+
+        text = response.text
+        header = '[\\"garturlres\\",\\"'
+        footer = '\\",'
+        if header not in text:
+            return {"status": False, "error": f"Header not found in response: {text}"}
+        start = text.split(header, 1)[1]
+        if footer not in start:
+            return {"status": False, "error": "Footer not found in response."}
+        url = start.split(footer, 1)[0]
+        return {"status": True, "url": url}
+    except Exception as e:
+        return {"status": False, "error": str(e)}
+
+
+def decode_google_news_url(source_url):
+    try:
+        url = requests.utils.urlparse(source_url)
+        path = url.path.split("/")
+        if (
+            url.hostname == "news.google.com"
+            and len(path) > 1
+            and path[-2] == "articles"
+            or "read"
+        ):
+            base64_str = path[-1]
+            decoded_bytes = base64.urlsafe_b64decode(base64_str + "==")
+            decoded_str = decoded_bytes.decode("latin1")
+
+            prefix = b"\x08\x13\x22".decode("latin1")
+            if decoded_str.startswith(prefix):
+                decoded_str = decoded_str[len(prefix) :]
+
+            suffix = b"\xd2\x01\x00".decode("latin1")
+            if decoded_str.endswith(suffix):
+                decoded_str = decoded_str[: -len(suffix)]
+
+            bytes_array = bytearray(decoded_str, "latin1")
+            length = bytes_array[0]
+            if length >= 0x80:
+                decoded_str = decoded_str[2 : length + 1]
+            else:
+                decoded_str = decoded_str[1 : length + 1]
+
+            if decoded_str.startswith("AU_yqL"):
+                return fetch_decoded_batch_execute(base64_str)
+
+            return {"status": True, "url": decoded_str}
+        else:
+            return {"status": False, "error": "Invalid Google News URL"}
+    except Exception as e:
+        return {"status": False, "error": str(e)}
+
+
+# # Example usage
+# if __name__ == "__main__":
+#     # source_url = "https://news.google.com/rss/articles/CBMiVkFVX3lxTE4zaGU2bTY2ZGkzdTRkSkJ0cFpsTGlDUjkxU2FBRURaTWU0c3QzVWZ1MHZZNkZ5Vzk1ZVBnTDFHY2R6ZmdCUkpUTUJsS1pqQTlCRzlzbHV3?oc=5"
+#     source_url = "https://news.google.com/rss/articles/CBMiqwFBVV95cUxNMTRqdUZpNl9hQldXbGo2YVVLOGFQdkFLYldlMUxUVlNEaElsYjRRODVUMkF3R1RYdWxvT1NoVzdUYS0xSHg3eVdpTjdVODQ5cVJJLWt4dk9vZFBScVp2ZmpzQXZZRy1ncDM5c2tRbXBVVHVrQnpmMGVrQXNkQVItV3h4dVQ1V1BTbjhnM3k2ZUdPdnhVOFk1NmllNTZkdGJTbW9NX0k5U3E2Tkk?oc=5"
+
+#     decoded_url = decode_google_news_url(source_url)
+#     print(decoded_url)
--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="googlenewsdecoder",
-    version="0.1.1",
-    description="A package to decode Google News URLs.",
+    version="0.1.2",
+    description="A python package to decode Google News URLs.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/SSujitX/google-news-url-decoder",
@@ -12,7 +12,7 @@ setup(
     license="MIT",
     packages=find_packages(),
     install_requires=[
-        "requests",  # Add any dependencies here
+        "requests",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
